7d3813ccc9e1ad849a5e65358ab96a0b995f1f85
oh306028 <hamerlao@gmail.com>
2025-12-01 19:31:17 +0100

    added new event logic

---
+++ Thesis.app/EventHandlers/LogInHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thesis.app.Events;
+using Thesis.app.Services;
+using Thesis.data;
+
+namespace Thesis.app.EventHandlers
+{
+    public class LogInHandler : INotificationHandler<LogInEvent>
+    {
+        public AppDbContext DbContext { get; }
+        public IAchievementService AchievementService { get; }
+
+        public LogInHandler(AppDbContext dbContext, IAchievementService achievementService)
+        {
+            DbContext = dbContext;
+            AchievementService = achievementService;
+        }
+        public async Task Handle(LogInEvent notification, CancellationToken cancellationToken)
+        {
+            await AchievementService.CheckStudentLogginAwards(notification.StudentId, notification.LogInDate);
+        }
+    }
+}
---
+++ Thesis.app/Events/LogInEvent.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.app.Events
+{
+    public class LogInEvent : INotification
+    {
+        public int StudentId { get; set; }
+        public DateTime LogInDate { get; set; }
+        public LogInEvent(int studentId, DateTime logInDate)
+        {
+            StudentId = studentId;
+            LogInDate = logInDate;
+        }
+    }
+}
