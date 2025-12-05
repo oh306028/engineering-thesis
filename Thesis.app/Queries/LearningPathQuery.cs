82018bf4beaee9c274c82f5802552c4c734fc0fa
oh306028 <hamerlao@gmail.com>
2025-12-05 23:59:56 +0100

    created logic for adaptive system

--- Thesis.app/Queries/LearningPathQuery.cs
+++ Thesis.app/Queries/LearningPathQuery.cs
@@ -6,8 +6,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Thesis.api.Extensions;
+using Thesis.app.Services;
 using Thesis.data;
 using Thesis.data.Data;
+using Thesis.data.Enums;
 using Thesis.data.Interfaces;
 
 
@@ -17,9 +19,10 @@ namespace Thesis.app.Queries
     {
         public class GetList : IRequest<List<LearningPath>>
         {
-            public GetList()
+            public LearningPathType Type { get; set; }
+            public GetList(LearningPathType type)
             {
-                
+                Type = type;
             }
         }
 
@@ -29,6 +32,15 @@ namespace Thesis.app.Queries
             public GetExercises(string publicId)
             {
                 PublicId = publicId;
+            }
+        }
+
+        public class GetHardestExercises : IRequest<List<Exercise>>
+        {
+            public int StudentId { get; set; }
+            public GetHardestExercises(int studentId)
+            {
+                StudentId = studentId;
             }
         }
     }
@@ -44,7 +56,7 @@ namespace Thesis.app.Queries
 
         public async Task<List<LearningPath>> Handle(LearningPathQuery.GetList request, CancellationToken cancellationToken)
         { 
-            return await DbContext.LearningPaths.AsNoTracking().ToListAsync(cancellationToken);
+            return await DbContext.LearningPaths.Include(p => p.Badges).AsNoTracking().Where(p => p.Type == (int)request.Type).ToListAsync(cancellationToken);
         }
     }
 
@@ -63,6 +75,20 @@ namespace Thesis.app.Queries
                 .AsNoTracking()
                 .Where(p => p.LearningPathExercises.Any(p => p.LearningPath.PublicId.ToString() == request.PublicId))
                 .ToListAsync();
+        }
+    }
+
+    public class GetHardestExercisesHandler : IRequestHandler<LearningPathQuery.GetHardestExercises, List<Exercise>>
+    {
+        public IAdaptiveSystemService AdaptiveSystem { get; }
+        public GetHardestExercisesHandler(IAdaptiveSystemService adaptiveSystem)
+        {
+            AdaptiveSystem = adaptiveSystem;
+        }
+
+        public async Task<List<Exercise>> Handle(LearningPathQuery.GetHardestExercises request, CancellationToken cancellationToken)
+        {
+            return await AdaptiveSystem.GetHardestExercises(request.StudentId);
         }
     }
 }
---
+++ Thesis.app/Services/AdaptiveSystemService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Thesis.data;
+using Thesis.data.Data;
+
+namespace Thesis.app.Services
+{
+    public interface IAdaptiveSystemService
+    {
+        Task<List<Exercise>> GetHardestExercises(int studentId);
+    }
+
+    public class AdaptiveSystemService : IAdaptiveSystemService
+    {
+        public AppDbContext DbContext { get; }
+        public AdaptiveSystemService(AppDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public async Task<List<Exercise>> GetHardestExercises(int studentId)
+        {
+            var notFinishedExercises = await DbContext.StudentExercises
+                .Include(p => p.Student)
+                .Include(p => p.Exercise)
+                .Where(p => !p.IsCompleted && p.StudentId == studentId)
+                .ToListAsync();
+
+            var hardestExerciseIds = notFinishedExercises
+             .Select(p => new
+             {
+                 p.ExerciseId,  
+                 Score =
+                     (p.Exercise.Level ?? 1) * 2 +
+                     p.Exercise.Points * 1 +
+                     p.WrongAnswers * 3 +
+                     p.Attempts * 2 +
+                     (p.LastAttempt.HasValue ? 1 : 0)
+             })
+             .OrderByDescending(x => x.Score)
+             .Take(5)
+             .Select(p => p.ExerciseId)
+             .ToList();
+
+            var exercises = await DbContext.Exercises
+                .Where(p => hardestExerciseIds.Contains(p.Id))
+                .ToListAsync();
+
+            return exercises;
+
+        }
+    }
+}
