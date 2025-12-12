9f2723644d1335fc7927620910c9ee8921ab7542
oh306028 <hamerlao@gmail.com>
2025-12-12 15:44:42 +0100

    added resolver for checking the current user for list

---
+++ Thesis.api/Resolvers/StudentCurrentUserResolver.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Thesis.app.Dtos.Student;
+using Thesis.data.Data;
+using Thesis.data;
+using System.Security.Claims;
+
+namespace Thesis.api.Resolvers
+{
+    public class StudentCurrentUserResolver : IValueResolver<Student, StudentDetails, bool>
+    {
+        private readonly AppDbContext dbContext;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public StudentCurrentUserResolver(AppDbContext dbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            this.dbContext = dbContext;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public bool Resolve(Student source, StudentDetails destination, bool destMember, ResolutionContext context)
+        {
+           return source.Id == int.Parse(httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        }
+    }   
+}
