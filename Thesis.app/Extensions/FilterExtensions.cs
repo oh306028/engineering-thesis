19d7ed50ca6b96db870df9c4db7defb9d3688dec
oh306028 <hamerlao@gmail.com>
2025-12-17 22:20:23 +0100

    filter extension added

---
+++ Thesis.app/Extensions/FilterExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thesis.data.Interfaces;
+
+namespace Thesis.app.Extensions
+{
+    public static class FilterExtensions
+    {
+        public static IQueryable<T> Filter<T>(
+        this IQueryable<T> query,   
+            IFIlter<T> filter)
+        {
+            var predicate = filter.GetPredicate();
+
+            return query.Where(predicate);
+        }
+    }
+}
