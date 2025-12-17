07d520e320b3ce875363ddf16323bfbf41b7acb9
oh306028 <hamerlao@gmail.com>
2025-12-17 21:32:42 +0100

    pagination feature added

---
+++ Thesis.app/Pagination/PaginationResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.app.Pagination
+{
+    public class PaginationResult<T>    
+    {
+        public List<T> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+
+        public PaginationResult()
+        {
+            Items = new List<T>();
+        }
+    }   
+}
