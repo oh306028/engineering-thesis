b54b1d4f7d07ab0634b0e09c092d49cc786615c3
oh306028 <hamerlao@gmail.com>
2025-12-17 22:23:30 +0100

    added new routes and components

--- Thesis.web/src/App.tsx
+++ Thesis.web/src/App.tsx
@@ -4,6 +4,16 @@ import Register from "./modules/account/views/Register";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
+import Dashboard from "./dashboard";
+import StudentDashboard from "./modules/student/views/studentDashboard";
+import { ToastContainer } from "react-toastify";
+import TeacherDashboard from "./modules/teacher/views/teacherDashboard";
+import ParentDashboard from "./modules/parent/views/parentDashboard";
+import RegisterTeacher from "./modules/account/views/RegisterTeacher";
+import AdminDashboard from "./modules/admin/views/adminDashboard";
+import { useEffect } from "react";
+import AccountService from "./modules/account/accountService";
+import ProfileDetails from "./components/ProfileDetails";
 
 function App() {
   const navigate = useNavigate();
@@ -33,13 +43,38 @@ function App() {
   );
   [navigate];
 
+  const verifyUserCredits = async () => {
+    try {
+      await AccountService.Verify();
+      navigate("/dashboard");
+    } catch (ex: any) {
+      localStorage.removeItem("token");
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    verifyUserCredits();
+  }, []);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
+        <Route path="/account/profile-details" element={<ProfileDetails />} />
         <Route path="/accounts/login" element={<Login />} />
         <Route path="/accounts/register" element={<Register />} />
+        <Route
+          path="/accounts/register-teacher"
+          element={<RegisterTeacher />}
+        />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/student" element={<StudentDashboard />} />
+        <Route path="/dashboard/teacher" element={<TeacherDashboard />} />
+        <Route path="/dashboard/parent" element={<ParentDashboard />} />
+        <Route path="/dashboard/admin" element={<AdminDashboard />} />
       </Routes>
+      <ToastContainer />
     </>
   );
 }
